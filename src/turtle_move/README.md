7fee1a653cae37c4ebdc43b27c763ccc42c63d22
Jack Erick <jackerick0212@gmail.com>
2025-01-28 11:44:11 -0500

    The turtle_move package is able to be executed with turtle_node and runs a counter

--- src/turtle_move/README.md
+++
@@ -1,4 +0,0 @@
-# CPS 498: Publishers and Subscriber Activity
-
-This is the move turtle_move package with the turtle_node as it's executable name.
-
--- src/turtle_move/setup.py
+++ src/turtle_move/setup.py
@@ -14,9 +14,9 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='jack',
-    maintainer_email='<jackerick0212@gmail.com>',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='jackerick0212@gmail.com',
+    description='Package for moving turtles in ROS 2',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
