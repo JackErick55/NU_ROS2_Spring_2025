4a6d5ea2d1291a41323a8dc57a698376547295de
Jack Erick <jackerick0212@gmail.com>
2025-01-28 08:37:22 -0500

    The first 3 steps of the Publishers and Subscriber Activityhas been completed

---
+++ src/turtle_move/README.md
@@ -0,0 +1,4 @@
+# CPS 498: Publishers and Subscriber Activity
+
+This is the move turtle_move package with the turtle_node as it's executable name.
+
--- src/turtle_move/setup.py
+++ src/turtle_move/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "turtle_node = turtle_move.turtle_node:main"
         ],
     },
 )
