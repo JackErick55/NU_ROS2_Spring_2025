37e1f55d819d45e07bfa24a3afbfcb3ef15aac55
Jack Erick <jackerick0212@gmail.com>
2025-01-30 17:47:40 -0500

    Parameters in the launch file

--- src/turtle_move/turtle_move/turtle_node.py
+++ src/turtle_move/turtle_move/turtle_node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Vector3
 from turtlesim.msg import Pose 
 
 rate_hz = 10
@@ -11,24 +11,42 @@ class TurtleNode(Node):
 
     def __init__(self):
         super().__init__("turtle_node")
-        self.pub_ = self.create_publisher(
-            Twist, "/turtle1/cmd_vel", 10)
         self.get_logger().info("This is turtle_node")
-        self.sub_ = self.create_subscription(
+        self._velocity = 2.0
+
+        self.declare_parameter("xmin", 2.0)
+        self.declare_parameter("xmax", 7.0)
+
+        self._xmin = self.get_parameter("xmin").value
+        self._xmax = self.get_parameter("xmax").value
+        self.get_logger().debug(f"Xmin: {self._xmin} Xmax: {self._xmax}")
+        
+        self.pub_ = self.create_publisher(
+            Twist, "cmd_vel", 10)
+
+        self.pub_.publish(Twist(linear=Vector3(x=self._velocity)))
+
+        self.get_logger().info("This is turtle_node")
+        self._sub = self.create_subscription(
             Pose, "/turtle1/pose", self.pose_callback, 10)
 
-    def pose_callback(self, pose: Pose):
-        cmd = Twist()
-        if pose.x > 7.0 or pose.x < 2.0 or pose.y > 7.0 or pose.y < 2.0:
-            cmd.linear.x = 1.0
-            cmd.angular.z = 0.9
-        else:
-            cmd.linear.x = 5.0
-            cmd.angular.z = 0.0
-        self.pub_.publish(cmd)
-        self.get_logger().info(
-            f"Received pose: x={pose.x}, y={pose.y}, theta={pose.theta}")
+    def pose_callback(self, pose):
+        if pose.x > self._xmax and self._velocity > 0:
+            self._velocity *= -1.0
+        elif pose.x < self._xmin and self._velocity < 0:
+            self._velocity *= -1.0
 
+        cmd = Twist()
+        cmd.linear.x = self._velocity  # Set linear velocity
+        cmd.angular.z = 0.0  # No angular velocity (straight line movement)
+
+        # Publish the command to control the turtle
+        self.pub_.publish(cmd)
+
+        self.get_logger().info(f"Pose: x={pose.x}, y={pose.y}, theta={pose.theta}, Publishing cmd: linear.x={cmd.linear.x}, angular.z={cmd.angular.z}")
+
+
+        
 
 def main(args=None):
     rclpy.init(args=args)
