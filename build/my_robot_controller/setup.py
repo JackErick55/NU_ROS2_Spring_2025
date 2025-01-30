8ff724a3d470beb6d2db3496865b9dd278cdb536
Jack Erick <jackerick0212@gmail.com>
2025-01-30 14:37:11 -0500

    I have accomplished the first task of the Publisher and Subscriber Activity

---
+++ build/my_robot_controller/setup.py
@@ -0,0 +1,1 @@
+/home/jack/ros2_ws/src/my_robot_controller/setup.py
--- src/turtle_move/turtle_move/turtle_node.py
+++ src/turtle_move/turtle_move/turtle_node.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+rate_hz = 10
+time_period = 1/rate_hz
 
 class TurtleNode(Node):
 
     def __init__(self):
         super().__init__("turtle_node")
-        self.counter_ = 0
-        self.create_timer(1.0, self.timer_callback)
+        self.pub_ = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
+        self.timer_ = self.create_timer(time_period, self.send_velocity_command)
+        self.get_logger().info("This is the turtle_node")
 
-    def timer_callback(self):
-        self.get_logger().info("Hello " + str(self.counter_))
-        self.counter_ += 1
-
+    def send_velocity_command(self):
+        msg = Twist()
+        msg.linear.x = 0.1
+        msg.angular.z = 0.0
+        self.pub_.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
